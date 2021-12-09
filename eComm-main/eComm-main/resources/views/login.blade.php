f644c1e3e95a7d72b755d60780622b9b2513577d
unknown <debnath.p.slg@gmail.com>
2021-12-09 14:07:55 +0530

    202112091406

---
+++ eComm-main/eComm-main/resources/views/login.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('container')
+
+<h1>Login Page</h1>
+<button class="btn btn-primary">Click here</button>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('container')
+
+<h1>Login Page</h1>
+<button class="btn btn-primary">Click here</button>
+@endsection
